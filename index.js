49c01c2ab56d2e772fea8dd0c52d0fef102b898a
Niklas Merz <niklasmerz@linux.com>
2019-09-04 21:33:36 +0200

    Use cookies to remember origin

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ addEventListener('fetch', event => {
     event.respondWith(handleRequest(event.request))
 })
 
+const COOKIEKEY = "cfwproxylasthost";
+
 /**
  * Respond to the request
  * @param {Request} request
@@ -14,13 +16,39 @@ async function handleRequest(request) {
         if (url.pathname === "/") {
             return fetch("https://niklas.merz.dev/proxy");
         } else {
-            console.error("Proxy", url);
-            return fetch(request).catch((err) => console.error("Proxy pass through error", err));
+            if (request.headers.has("Cookie")) {
+                url.hostname = parseCookies(request)[COOKIEKEY];
+                return fetch(url, { method: request.method });
+            } else {
+                return fetch(request, { method: request.method }).catch((err) => console.error("Proxy pass through error", err));
+            }
         }
     }
 
-    return fetch(params.get("url")).catch((e) => {
-        console.error(e);
-        return new Response("Not found", { status: 404 });
-    })
+    return fetch(params.get("url"))
+        .then((res) => {
+            let headers = new Headers(res.headers);
+            const resUrl = new URL(res.url);
+            headers.append("Set-Cookie", COOKIEKEY + "=" + resUrl.hostname);
+            res = new Response(res.body, { headers: headers, status: res.status });
+            return res;
+        })
+        .catch((e) => {
+            console.error(e);
+            return new Response("Not found", { status: 404 });
+        })
+}
+
+
+//https://stackoverflow.com/questions/3393854/get-and-set-a-single-cookie-with-node-js-http-server/6980804
+function parseCookies(request) {
+    var list = {},
+        rc = request.headers.get("Cookie");
+
+    rc && rc.split(';').forEach(function (cookie) {
+        var parts = cookie.split('=');
+        list[parts.shift().trim()] = decodeURI(parts.join('='));
+    });
+
+    return list;
 }
